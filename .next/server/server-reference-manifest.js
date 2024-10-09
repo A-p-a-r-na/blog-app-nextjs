6047d0b767ea998e2c8bdec1017e4ee6b6c2e719
Aparna <aparna@dbizsolution.com>
2024-10-09 21:52:48 +0530

    Changed the style

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"TKOmep0YDPWZQ1qayDb95s7EeSXeMXU+dEiXjLHlfb0=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"fDaEKLWG+BJikXOAPwbiJ2hQXDn1//ulIg6Jyb066EU=\"\n}"
